d5fa749a8ee185536c6b48196c1ba255060aed5e
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-10-26 13:06:10 +0100

    data fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { InfoListComponent } from 'src/components/info-list/info-list.component';
 import { LayoutComponent } from 'src/components/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[LayoutComponent],
+  imports:[LayoutComponent, InfoListComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
--- src/components/info-list/info-list.component.ts
+++ src/components/info-list/info-list.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 @Component({
   selector: 'app-info-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './info-list.component.html',
   styleUrls: ['./info-list.component.css']
 })
 export class InfoListComponent implements OnInit {
 
-  constructor() { }
+  user:any = []
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get("https://www.reddit.com/r/todayilearned/hot.json?limit=25").subscribe((res) => {
+      console.log(res)
+      this.user = res
+    })
   }
 
 }
